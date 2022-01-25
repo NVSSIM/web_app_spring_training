ff6fb7c15327d2927323a4d63f27264a3bed5de8
NVSSIM <nassim.joudar@datavalue-consulting.com>
2022-01-25 22:07:37 +0100

    Final Commit TP1

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -1,23 +1,23 @@
 package fr.lernejo.todo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class TodoListController {
 
-    private final List<Todo> todos = new ArrayList<>();
+    private final TodoRepository repository;
+    public TodoListController(TodoRepository todoRepository){
+        this.repository = todoRepository;
+    }
+
     @PostMapping("/api/todo")
-    void addTodo(Todo todo){
-        todos.add(todo);
+    public void post(@RequestBody TodoEntity todoEntity){
+        this.repository.save(todoEntity);
     }
     @GetMapping("/api/todo")
-    List<Todo> getTodos(){
-        return todos;
+    public Iterable<TodoEntity> get(){
+        return this.repository.findAll();
     }
 }
+
